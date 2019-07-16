858e3405d1a87c43fc25077bd72c1ceafaf96ab5
Sergio Cosme <checholin87@gmail.com>
2019-07-15 23:07:08 -0500

    float placeholder

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app" class="container-fluid">
+    <div>
+      <form action="#">
+        <div class="float-placeholder">
+          <input type="text" name="email" required>
+          <label for="email">Email</label></div>
+      </form>
+    </div>
     <div class="row">
       <div class="col col-sm-8 offset-sm-2">
         <pdf src="./pdf-sample.pdf"></pdf>
